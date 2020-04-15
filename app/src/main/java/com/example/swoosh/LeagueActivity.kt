53cfda1acca8031fdb4121e5e0ef9d61bc94956e
Svetozar Pavlovic <s.pavlovic@levi9.com>
2020-04-15 19:27:53 +0200

    feat: Add Skill activity

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun goToSkillActivity(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
+
     }
 }
--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "${javaClass.simpleName} onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGetStarted.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
+    }
+    override fun onPause() {
+        Log.d(TAG, "${javaClass.simpleName} onPause")
+        super.onPause()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "${javaClass.simpleName} onResume")
+        super.onResume()
     }
 }
