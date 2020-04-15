cf8ec03748d1d7448656fecb636d1ba8d02d9ae5
Svetozar Pavlovic <s.pavlovic@levi9.com>
2020-04-15 20:26:40 +0200

    setup: Add launcher icon assets

--- app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -1,12 +1,19 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SkillActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+    }
+
+    fun goToFinishActivity(view: View) {
+        val finishActivity = Intent(this, FinishActivity::class.java)
+        startActivity(finishActivity)
     }
 }
